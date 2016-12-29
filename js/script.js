9eeb1fb9e541b01f90025df9ac3a3086ba53ec72
Natalya Ivanova <natalyas.ivanova@gmail.com>
2016-12-29 13:30:19 +0200

    Get num of posts from Object

--- js/script.js
+++ js/script.js
@@ -1,8 +1,37 @@
 $(function() {
-    $.ajax({
-        url: "https://webhose.io/search?token=e2e94dc5-1ce2-49d4-a8d9-4932418149cb&format=json&q=sherlock",
-        success: function(data) {
-            console.log(data);
+
+    var query;
+    var body = $("body");
+    var form = $("form");
+    var searchbox = $("input[type=text]");
+    var submit = $("input[type=submit]");
+
+    searchbox.focus();
+
+    searchbox.keydown(function(e) {
+        if (e.which == 13) {
+            query = $(this).val();
+            // $(this).change();
+            submit.focus();
+            form.submit();
+            body.append($("<p>Searching for " + query + "...</p>"));
+            body.append($("<p>Complete!</p>"));
         }
     });
+
+    $("form").submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: "https://webhose.io/search?token=e2e94dc5-1ce2-49d4-a8d9-4932418149cb&format=json&q=" + query,
+            success: function(data) {
+                var heading_message = $("<p></p>");
+                var text = "Found <b>" + data.totalResults + "</b> posts matching your filters from the past <b>3 days</b>.";
+                text += " <br/>The posts are ordered for consumption by crawl date, from oldest to newest.";
+                heading_message.html(text);
+                body.append(heading_message);
+                console.log(data);
+            }
+        });
+    });
+
 });

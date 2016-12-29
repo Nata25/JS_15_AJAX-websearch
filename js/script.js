09098db4a310c4462c703c1beeabab74a7e2c057
Natalya Ivanova <natalyas.ivanova@gmail.com>
2016-12-30 00:29:58 +0200

    Add about and brief; edit intro

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ $(function() {
     var form = $("form");
     var searchbox = $("input[type=text]");
     var submit = $("input[type=submit]");
+    var results = $(".results");
 
     searchbox.focus();
 
@@ -13,7 +14,7 @@ $(function() {
             query = $(this).val();
             $(this).change();
             submit.focus();
-            body.append($('<p>Searching for "' + query + '"...</p>'));
+            results.html("Searching for " + query + "...");
         }
     });
 
@@ -22,46 +23,66 @@ $(function() {
         $.ajax({
             url: "https://webhose.io/search?token=e2e94dc5-1ce2-49d4-a8d9-4932418149cb&format=json&q=" + query,
             success: function(data) {
-                body.append($("<p>Complete!</p>"));
-
-                var len = (data.totalResults > 10) ? 10 : data.total;
-                var results = $("<section class='results'></section>");
+                results.html("");
+                var len = (data.totalResults > 20) ? 20 : data.total;
                 var heading_message = $("<p></p>");
-                var text = "Found <span class='strong'>" + data.totalResults + "</span> posts matching your filters from the past <span class='strong'>3 days</span>.";
-                text += " <br/>The posts are ordered for consumption by crawl date, from oldest to newest.";
-                heading_message.html(text);
+                var intro = "<p>Found <span class='strong'>" + data.totalResults + "</span> posts matching your filters from the past <span class='strong'>3 days</span></p>";
+                intro += "<p><span class='strong'>" + len + "</span> results of total are shown.</p>";
+                intro += "<p>The posts are ordered for consumption by crawl date, from oldest to newest.</p>";
+                heading_message.html(intro);
                 results.append(heading_message);
-                body.append(results);
 
                 for (var i = 0; i < len; i++) {
-                    var img = (data.posts[i].thread.main_image) ? data.posts[i].thread.main_image : "https://webhose.io/public/images/noimage.png";
-                    var url = data.posts[i].thread.url;
-                    var entity = createEntity(img, data.posts[i].thread.title, url);
-                    console.log(entity);
+                    var item = data.posts[i];
+                    var img = (item.thread.main_image) ? item.thread.main_image : "https://webhose.io/public/images/noimage.png";
+                    var published = parseDate(item.thread.published);
+                    var entity = createEntity(img,
+                        item.thread.title,
+                        item.thread.url,
+                        published,
+                        item.author,
+                        item.thread.site,
+                        item.text);
+
                     results.append(entity);
                 }
-
                 console.log(data);
             }
         });
     });
 
     // function createEntity(img, title, url, pub, author, brief, lang, num, total, country, type, score) {
-    function createEntity(img, title, url) {
-
-
-
-        var html = $("#NY-carousel-template").html();
-        var renderedCarousel = tmpl(html, NYcarouselContent);
-        nyCarousel.append(renderedCarousel)
-
+    function createEntity(img, title, url, pub, author, site, text) {
 
         var container = $("<div class='entity'></div>");
         container.append($("<div class='preview_img'></div>").append($("<img src='" + img + "'>")));
         var text_container = $("<div class='text_container'></div>");
         container.append(text_container);
         text_container.append($("<a href='" + url + "'>" + title + "</a>"));
+        var about = $("<p class='about'><span class='strong'>Published at</span>: "
+                    + pub
+                    + " <span class='strong'>By:</span> " + author
+                    + " <span class='strong'>@</span> " + site
+                    + "</p>");
+        text_container.append(about);
+        var brief = $("<p class='text'></p>");
+        brief.text(text);
+        text_container.append(brief);
         return container;
+    }
+
+    // Parse Date() into m/d/yy h:mm A/PM string
+    function parseDate(dt) {
+        var date = new Date(dt);
+        var result = '';
+        result += (date.getMonth() + 1);
+        result += "/";
+        result += date.getDate() + "/" + date.getFullYear();
+        result += " " + (date.getHours() % 12) + ":" + date.getMinutes() + " ";
+        var meridiem = (date.getHours() < 11) ? "AM" : "PM";
+        result += meridiem;
+
+        return result;
     }
 
 });
